dd21042de5051b384d548b8c24ffbfcf8d815291
Ankit Agrawal <ankitagrawal1991@gmail.com>
2019-04-18 19:44:42 +0530

    Moved project from SpringMVC to Jersey 2

--- src/main/java/com/thecodingenthusiast/learning/controller/Home.java
+++ src/main/java/com/thecodingenthusiast/learning/controller/Home.java
@@ -1,18 +1,16 @@
 package com.thecodingenthusiast.learning.controller;
 
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/hello")
 public class Home {
     private static final Logger LOG = Logger.getLogger(Home.class);
 
-    @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public ModelAndView get() {
-        LOG.info("Hello there..");
-        return new ModelAndView("hello", "user", "Ankit");
+    @GET
+    public String getMsg() {
+        return "Hello World !! - Jersey 2.28";
     }
 }
